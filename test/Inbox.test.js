2d86a852638bc5a3109f486c39b3f46c16d32dd7
NilMV <nil.medvedev17@gmail.com>
2018-01-11 00:25:58 +0300

    fix bug of web3 and finish tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,6 +7,8 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
+const EDITED_STRING = 'New string there!';
 
 beforeEach (async () => {
   //Get a list of all accounts
@@ -15,12 +17,26 @@ beforeEach (async () => {
   //the contract
 
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
     .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can set message', async () => {
+    await inbox.methods.setMessage(EDITED_STRING).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, EDITED_STRING);
   });
 });
+
+
+
